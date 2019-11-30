00cc840f11cf324fcf648c343fb24c9dc09f22f9
Amina Mustafic <amina.mustafiic@gmail.com>
2019-11-30 23:26:45 +0100

    Added ArrayList and loops for transfer

--- src/Test.java
+++ src/Test.java
@@ -1,42 +1,53 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Test {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+
 		// objekti
-		KreiranjeRacuna racun1 = new KreiranjeRacuna(21235, "Amina", 100);
-		KreiranjeRacuna racun2 = new KreiranjeRacuna(21236, "Belma", 50);
-//		KreiranjeRacuna racun3 = new KreiranjeRacuna(21236, "Belma", 50);
-		
+		KreiranjeRacuna racun1 = new KreiranjeRacuna(12345, "Amina", 100);
+		KreiranjeRacuna racun2 = new KreiranjeRacuna(12346, "Belma", 50);
+
 		// lista racuna
 		ArrayList<KreiranjeRacuna> racuni = new ArrayList<>();
 		racuni.add(racun1);
 		racuni.add(racun2);
-		
+
 		// provjera da li racun ima isti broj
-		for(int i=0; i<racuni.size(); i++) {
-			for(int j=0; j<racuni.size(); j++) {
-				if(racuni.get(i).getBrojRacuna() == racuni.get(j).getBrojRacuna()) {
+		for (int i = 0; i < racuni.size(); i++) {
+			for (int j = i + 1; j < racuni.size(); j++) {
+				if (racuni.get(i).getBrojRacuna() == racuni.get(j).getBrojRacuna()) {
 					throw new ArithmeticException("Broj racuna " + racuni.get(j).getBrojRacuna() + " nije validan!");
 				}
 			}
 		}
-		
-		//TRANSFER
+
+		// kreiranje fajla sa svim racunima
+		Path path = Paths.get("brojevi.txt");
+
+		if (!Files.exists(path)) {
+			Files.createFile(path);
+		}
+
+		// TRANSFER
 		TransferNovca transfer = new TransferNovca();
 		Scanner input = new Scanner(System.in);
-		System.out.println("Unesite racun sa kojeg prebacujete novac: ");
+		System.out.print("Unesite racun sa kojeg prebacujete novac: ");
 		int source = input.nextInt();
-		System.out.println("Unesite racun na koji prebacujete novac: ");
+		System.out.print("Unesite racun na koji prebacujete novac: ");
 		int target = input.nextInt();
-		System.out.println("Unesite iznos koji zelite prebaciti: ");
+		System.out.print("Unesite iznos koji zelite prebaciti: ");
 		double iznos = input.nextDouble();
 		System.out.println();
 		input.close();
 		boolean transf = false;
-		
+
 		for (int i = 0; i < racuni.size(); i++) {
 			for (int j = 0; j < racuni.size(); j++) {
 				if (racuni.get(j).getBrojRacuna() == target && racuni.get(i).getBrojRacuna() == source) { // da li postoje u sistemu
@@ -57,7 +68,7 @@ public class Test {
 				}
 			}
 		}
-		
+
 		if (!transf) {
 			System.out.println("-----NEUSPJESAN TRASNFER-----");
 		}
