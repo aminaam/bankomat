0fe74bbff2f9e6aee2adfc337c6bb888e3db0008
Amina Mustafic <amina.mustafiic@gmail.com>
2019-11-30 22:45:55 +0100

    Created Arraylist and new exception in loop

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Test {
 
@@ -6,6 +8,31 @@ public class Test {
 		// objekti
 		KreiranjeRacuna racun1 = new KreiranjeRacuna(21235, "Amina", 100);
 		KreiranjeRacuna racun2 = new KreiranjeRacuna(21236, "Belma", 50);
+		
+		// lista racuna
+		ArrayList<KreiranjeRacuna> racuni = new ArrayList<>();
+		racuni.add(racun1);
+		racuni.add(racun2);
+		
+		// provjera da li racun ima isti broj
+		for(int i=0; i<racuni.size(); i++) {
+			for(int j=0; j<racuni.size(); j++) {
+				if(racuni.get(i).getBrojRacuna() == racuni.get(j).getBrojRacuna()) {
+					throw new ArithmeticException("Broj racuna " + racuni.get(j).getBrojRacuna() + " nije validan!");
+				}
+			}
+		}
+		
+		//TRANSFER
+		TransferNovca transfer = new TransferNovca();
+		Scanner input = new Scanner(System.in);
+		System.out.println("Unesite source racun: ");
+		int source = input.nextInt();
+		System.out.println("Unesite target raucn: ");
+		int target = input.nextInt();
+		System.out.println("Unesite iznos koji zelite prebaciti: ");
+		double iznos = input.nextDouble();
+		System.out.println();
 		
 
 	}
