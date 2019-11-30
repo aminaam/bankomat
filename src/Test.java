06657eec42b62612f71b9440fa7800480d87bf19
Amina Mustafic <amina.mustafiic@gmail.com>
2019-11-30 22:59:31 +0100

    Added loop for transfer

--- src/Test.java
+++ src/Test.java
@@ -8,6 +8,7 @@ public class Test {
 		// objekti
 		KreiranjeRacuna racun1 = new KreiranjeRacuna(21235, "Amina", 100);
 		KreiranjeRacuna racun2 = new KreiranjeRacuna(21236, "Belma", 50);
+//		KreiranjeRacuna racun3 = new KreiranjeRacuna(21236, "Belma", 50);
 		
 		// lista racuna
 		ArrayList<KreiranjeRacuna> racuni = new ArrayList<>();
@@ -26,15 +27,39 @@ public class Test {
 		//TRANSFER
 		TransferNovca transfer = new TransferNovca();
 		Scanner input = new Scanner(System.in);
-		System.out.println("Unesite source racun: ");
+		System.out.println("Unesite racun sa kojeg prebacujete novac: ");
 		int source = input.nextInt();
-		System.out.println("Unesite target raucn: ");
+		System.out.println("Unesite racun na koji prebacujete novac: ");
 		int target = input.nextInt();
 		System.out.println("Unesite iznos koji zelite prebaciti: ");
 		double iznos = input.nextDouble();
 		System.out.println();
+		input.close();
+		boolean transf = false;
 		
-
+		for (int i = 0; i < racuni.size(); i++) {
+			for (int j = 0; j < racuni.size(); j++) {
+				if (racuni.get(j).getBrojRacuna() == target && racuni.get(i).getBrojRacuna() == source) { // da li postoje u sistemu
+					transfer.setTargetAccount(target);
+					transfer.setSourceAccount(source);
+					if (racuni.get(i).getIznosNaRacunu() > iznos) { // da li je iznos dovoljan
+						racuni.get(i).isplata(iznos);
+						racuni.get(j).uplata(iznos);
+						System.out.println("-----USPJESAN TRANSFER-----\n");
+						racuni.get(i).ispisRacuna();
+						System.out.println("_________________________\n");
+						racuni.get(j).ispisRacuna();
+						transf = true;
+						break;
+					} else {
+						System.out.println("Ne moze se izvrsiti transfer! Na source racunu nema dovoljno novca.");
+					}
+				}
+			}
+		}
+		
+		if (!transf) {
+			System.out.println("-----NEUSPJESAN TRASNFER-----");
+		}
 	}
-
 }
